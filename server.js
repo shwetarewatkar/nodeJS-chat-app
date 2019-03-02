b3f8be68c41b0e4ed830e7fe1bfc53907e69d884
Shweta <shweta@ShwetaR.local>
2019-03-01 22:47:52 -0800

    socket connection

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
-
 var express = require('express')
+var bodyParser =  require('body-parser')
 var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 var messages = [
-    {name: "Robert", message:"Hello"},
-    {name: "Julia", message:"Hi!"}
+    {name: 'Robert', message:'Hello'},
+    {name: 'Julia', message:'Hi!'}
 ]
 
-app.get('/messages',(req,res) => {
+app.get('/messages',(req, res) => {
     res.send(messages)
 })
 
-var server = app.listen(3000, () => {
-    console.log("Server is listning on port", server.address().port)
+app.post('/messages',(req, res) => {
+    console.log(req.body)
+    messages.push(req.body)
+    res.sendStatus(200)
+}) 
+
+io.on('connection', (socket) => {
+    console.log('User connected')
+})
+var server = http.listen(3000, () => {
+    console.log("Server is listening on port", server.address().port)
 })
