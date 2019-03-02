cd97249745dbbed66f4a995089bdaf42d6d04721
Shweta <shweta@ShwetaR.local>
2019-03-01 21:27:32 -0800

    set up server

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ var express = require('express')
 var app = express()
 
 app.use(express.static(__dirname))
+
+var messages = [
+    {name: "Robert", message:"Hello"},
+    {name: "Julia", message:"Hi!"}
+]
+
+app.get('/messages',(req,res) => {
+    res.send(messages)
+})
+
 var server = app.listen(3000, () => {
     console.log("Server is listning on port", server.address().port)
 })
