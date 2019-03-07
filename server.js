cf1f29859a7f013ed6aec0868365c82928f48a64
shwetarewatkar <shweta.rewatkar2@gmail.com>
2019-03-06 21:36:00 -0800

    create socket.io event listener

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.get('/messages',(req, res) => {
 app.post('/messages',(req, res) => {
     console.log(req.body)
     messages.push(req.body)
+    io.emit('message',req.body)
     res.sendStatus(200)
 }) 
 
